56190371a83d4ad6249db3028ac1a7a61909575d
tera1707 <Masa1017>
2023-04-06 20:58:02 +0900

    初回

---
+++ AppCrashJikken/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppCrashJikken
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // メインスレッドで0除算
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var z = new ZeroJozan();
+
+            //引数4個目を0にしたら0除算発生する
+            z.DoJozan(1,2,3,0);
+        }
+    }
+}
---
+++ AppCrashJikken/ZeroJozan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCrashJikken
+{
+    internal class ZeroJozan
+    {
+        public ZeroJozan() { }
+
+        /// p4を0にしたら0除算発生する
+        public int DoJozan(int p1, int p2, int p3, int p4)
+        {
+            return SubFunc(p1 + p2 + p3, p4);
+        }
+
+        private int SubFunc(int bunshi, int bunbo)
+        {
+            bunshi = bunshi + 2;
+            bunshi = bunshi * 4;
+            int ret = bunshi / bunbo;
+            return ret;
+
+
+        }
+
+    }
+}
